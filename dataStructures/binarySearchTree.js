122200329584c7ca1578e0792a145de643b0c5f1
Maciej Cywinski <cyvvilek@gmail.com>
2022-11-14 20:05:59 +0100

    BST find

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -41,13 +41,34 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (!this.root) return false;
+
+    let current = this.root;
+    while(true) {
+      if (value === current.value) return current;
+
+      if (value < current.value) {
+        if (!current.left) return false;
+        current = current.left
+      } else {
+        if (!current.right) return false;
+        current = current.right;
+      }
+    }
+  }
 }
 
-let tree = new BinarySearchTree();
-tree.insert(7);
-tree.insert(10);
-tree.insert(7);
-tree.insert(6);
-tree.insert(8);
-tree.insert(4);
-console.log(tree);
+// let tree = new BinarySearchTree();
+// tree.insert(7);
+// tree.insert(10);
+// tree.insert(6);
+// tree.insert(8);
+// tree.insert(11);
+
+// console.log(tree.find(11));
+// console.log(tree.find(12));
+// console.log(tree.find(6));
+// console.log(tree.find(9));
+// console.log(tree);

96710e453bccdc7381e68a8d12562d18cbe8a4e0
Maciej Cywinski <cyvvilek@gmail.com>
2022-11-15 21:27:50 +0100

    quickSort

--- sort/quick.js
+++ sort/quick.js
@@ -1,1 +1,28 @@
-// todo
+const pivot = (arr, start = 0, end = arr.length) => {
+  let pivotPoint = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivotPoint > arr[i]) {
+      swapIdx++;
+      [arr[i], arr[swapIdx]] = [arr[swapIdx], arr[i]];
+    }
+  }
+
+  [arr[start], arr[swapIdx]] = [arr[swapIdx], arr[start]];
+  console.log(arr);
+  return swapIdx;
+};
+
+const quickSort = (arr, left = 0, right = arr.length) => {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+
+    quickSort(arr, left, pivotIndex - 1);
+
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+};
+
+// console.log(quickSort([4, 8, 2, 1, 5, 7, 2, 3]))

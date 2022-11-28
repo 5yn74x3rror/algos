4ca3e18692b0fddf119db691af3f1539dddac5fa
Maciej Cywinski <cyvvilek@gmail.com>
2022-11-28 15:03:02 +0100

    dfs + bfs

--- dataStructures/graph/index.js
+++ dataStructures/graph/index.js
@@ -25,4 +25,83 @@ class Graph {
     this.list[n2] = this.list[n2].filter(el => el !== n1);
     this.list[n1] = this.list[n1].filter(el => el !== n2);
   }
+
+  dfsRecursion(start) {
+    const result = [];
+    const visited = {};
+
+    (function traverse(node) {
+      if (node.length === 0) return;
+  
+      result.push(node);
+      visited[node] = true;
+      
+      const neighbours = this.list[node];
+      for (let i = 0; i < neighbours.length; i++) {
+        if (!visited[neighbours[i]]) {
+          traverse.call(this, neighbours[i]);
+        }
+      }
+    }).call(this, start);
+
+    return result;
+  }
+
+  dfsIteration(start) {
+    let stack = [];
+    const visited = {};
+    const result = [];
+    stack.push(start);
+
+    while(stack.length) {
+      const node = stack.pop();  // this makes it go different direction than revursive version
+      result.push(node);
+      if (!visited[node]) {
+        visited[node] = true;
+        stack = stack.concat(this.list[node]);
+      }
+    }
+
+    return result;
+  }
+
+  bfs(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {
+      start: true,
+    };
+
+    while (queue.length) {
+      const node = queue.shift();
+      result.push(node);
+      this.list[node].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+
+      });
+    }
+
+    console.log(result);
+  }
 }
+const g = new Graph();
+
+g.addNode('A');
+g.addNode('B');
+g.addNode('C');
+g.addNode('D');
+g.addNode('E');
+g.addNode('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+g.bfs('A');
